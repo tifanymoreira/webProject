deb62612a0e8e791fd71ce26fed1ad8de33b323a
Tifany Moreira <tiluiza12@gmail.com>
2025-05-27 20:05:00 -0300

    add files

---
+++ src/test/java/com/example/webfinalproject/WebFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
